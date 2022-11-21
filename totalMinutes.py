8147f35dee6210a5a389db714cb26740d82f4368
Paul <paul@DESKTOP-MOPT9KJ.localdomain>
2022-11-20 22:04:22 -0600

    clean up

--- totalMinutes.py
+++ totalMinutes.py
@@ -2,26 +2,24 @@ import sys, getopt, datetime
 
 
 def main(argv):
-
+    usage = "Usage: totalMinutes.py -s <Start Time> -e <End Time>"
+    dateTimeFormat = "%d/%b/%y %I:%M%p"
     try:
         opts, args = getopt.getopt(argv,"s:e:",["startTime=","endTime="])
     except getopt.GetoptError as err:
         print(err)
-        print ("Usage: totalMinutes.py -s <Start Time> -e <End Time>")
+        print (usage)
         sys.exit(2)
 
     if len(opts) == 0:
-        print("Usage: totalMinutes.py -s <Start Time> -e <End Time>")
+        print(usage)
         sys.exit(2)
-
-    startTime = ""
-    endTime = ""
 
     for opt, arg in opts:
         if opt == "-s":
-            startTime = datetime.datetime.strptime(arg,"%d/%b/%y %I:%M%p")
+            startTime = datetime.datetime.strptime(arg, dateTimeFormat)
         if opt == "-e":
-            endTime = datetime.datetime.strptime(arg,"%d/%b/%y %I:%M%p")
+            endTime = datetime.datetime.strptime(arg, dateTimeFormat)
 
     diff = endTime - startTime
     minutes = diff.total_seconds() / 60
